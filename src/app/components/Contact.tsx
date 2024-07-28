e4a57f90f6b03dba213095587587adc3252e4022
csablk <samircsa@mail.ru>
2024-07-28 08:21:47 +0300

    last

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -24,8 +24,8 @@ export default function Contact() {
 								<a href='https://www.google.com/maps/place/%D1%83%D0%BB.+%D0%93%D0%BE%D0%BB%D0%BE%D0%B2%D0%BA%D0%BE,+93,+%D0%9D%D0%B0%D0%BB%D1%8C%D1%87%D0%B8%D0%BA,+%D0%9A%D0%B0%D0%B1%D0%B0%D1%80%D0%B4%D0%B8%D0%BD%D0%BE-%D0%91%D0%B0%D0%BB%D0%BA%D0%B0%D1%80%D1%81%D0%BA%D0%B0%D1%8F+%D0%A0%D0%B5%D1%81%D0%BF.,+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F,+360017/@43.4894961,43.5831106,17z/data=!3m1!4b1!4m6!3m5!1s0x405a06b41d89699d:0xb398d93a6e24e75f!8m2!3d43.4894961!4d43.5831106!16s%2Fg%2F11vkgppthf?entry=ttu'>
 									ул. Головко 93
 								</a>
-								<div className={style.contactLine}>|</div>
 							</p>
+							<div className={style.contactLine}>|</div>
 						</li>
 						<li>
 							<p>
@@ -43,8 +43,8 @@ export default function Contact() {
 								>
 									goldmen.store07
 								</a>
-								<div className={style.contactLine}>|</div>
 							</p>
+							<div className={style.contactLine}>|</div>
 						</li>
 						<li className={style.phoneText}>
 							<p>
